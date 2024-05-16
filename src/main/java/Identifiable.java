e2568d1ce82b76accb5b119280aac9c26874d966
Niels van Kuppevelt <NC.vanKuppevelt@student.han.nl>
2024-05-16 13:45:13 +0200

    Add IdentifierCircle.java

--- src/main/java/Identifiable.java
+++ src/main/java/Identifiable.java
@@ -1,7 +1,16 @@
-public abstract class Identifiable {
+public abstract class Identifiable implements Comparable<Identifiable> {
     protected Comparable<String> identifier;
 
     protected Identifiable(String preHashIdentifier) {
         identifier = Sha1Hasher.hash(preHashIdentifier);
+    }
+
+    @Override
+    public int compareTo(Identifiable identifiable) {
+        return identifier.compareTo(identifiable.getIdentifier());
+    }
+
+    public String getIdentifier() {
+        return identifier.toString();
     }
 }
---
+++ src/main/java/IdentifierCircle.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class IdentifierCircle {
+    private List<Node> circle;
+
+    public void addNode(Node node) {
+
+    }
+
+    /*
+    Key k is assigned to the first node whose identifier is equal to or follows (the identifier of) k in the identifier
+    space.
+     */
+    public void addKey(Key k) {
+        for (Node node : circle) {
+        }
+    }
+}
