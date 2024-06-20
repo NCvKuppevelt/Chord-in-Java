a246b9de19aa3acda412e4fa9d7abd5925ad0ce8
Niels van Kuppevelt <NC.vanKuppevelt@student.han.nl>
2024-06-20 15:45:58 +0200

    Add name field to Key

--- src/main/java/Key.java
+++ src/main/java/Key.java
@@ -1,10 +1,12 @@
 public class Key extends Identifiable {
     private String value;
+    private String name;
 
-    public Key (String value) {
+    public Key (String value, String name) {
         // "a key identifier is produced by hashing the key", p3
         super(value);
         this.value = value;
+        this.name = name;
     }
 
     public String getValue() {
